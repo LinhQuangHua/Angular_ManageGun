239c824e9b7f20c419ab9ff8f08075cbc78f6671
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-06-12 23:41:57 +0700

    Add Toastr for Gun Form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,70 +11,70 @@ export class RecipeService {
       'Pistol-0001',
       'Beretta 92a',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $1400',
+      '1400',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-beretta-92.jpg'
     ),
     new Recipe(
       'Pistol-0002',
       '92FS',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $1000',
+      '1000',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-colt-m1911.jpg'
     ),
     new Recipe(
       'Pistol-0003',
       '92G',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $800',
+      '800',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-glock.jpg'
     ),
     new Recipe(
       'Pistol-0004',
       'Beretta 92a-12',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $1500',
+      '1500',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-beretta-92.jpg'
     ),
     new Recipe(
       'Pistol-0005',
       '92FS-2',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $12000',
+      '12000',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-colt-m1911.jpg'
     ),
     new Recipe(
       'Pistol-0006',
       '92G-T74',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $1800',
+      '1800',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-glock.jpg'
     ),
     new Recipe(
       'Pistol-0007',
       '92G',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $800',
+      '800',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-glock.jpg'
     ),
     new Recipe(
       'Pistol-0008',
       'Beretta 92a-12',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $1500',
+      '1500',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-beretta-92.jpg'
     ),
     new Recipe(
       'Pistol-0009',
       '92FS-2',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $12000',
+      '12000',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-colt-m1911.jpg'
     ),
     new Recipe(
       'Pistol-0010',
       '92G-T74',
       'Western Europe, Eastern Europe, Middle East & North Africa, Sub-Saharan Africa, Central Asia, South Asia, East Asia, Pacific, North America, Central America, South America',
-      'Price: $1800',
+      '1800',
       'http://www.smallarmssurvey.org/fileadmin/img/weapons-id/SAS-weapons-pistol-glock.jpg'
     ),
   ];
