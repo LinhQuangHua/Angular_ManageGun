c1efdc19f6c8d8b53012042995932171ee60b62f
Tô Phở Không Hành <vggff619@gmail.com>
2021-06-13 22:57:37 +0700

    Add launch.json (to run debug on Edge) & refactor unsued jQuery codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -42,6 +43,7 @@ import { AngularFireModule } from '@angular/fire';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
     ReactiveFormsModule,
